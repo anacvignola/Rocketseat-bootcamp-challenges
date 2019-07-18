a5240cbb0ef6db2fa92e7a56ab6f082392b64b70
anacvignola <anacvignola@gmail.com>
2019-07-18 17:47:46 -0300

    sitting String variable to compare and correcting some errors

--- challenge1/index.js
+++ challenge1/index.js
@@ -1,15 +1,15 @@
-const express = require("express");
+const express = require('express');
 const server = express();
 server.use(express.json());
 
 const projects = [];
 
-let req = 0;
+let logReq = 0;
 
 //conta requisções
 function requisitions(req, res, next) {
-  req++;
-  console.log(`Requesitions: ${req}`);
+  logReq++;
+  console.log(`Requesitions: ${logReq}`);
   return next();
 }
 
@@ -17,9 +17,9 @@ server.use(requisitions);
 
 function checkProjectExists(req, res, next) {
   const { id } = req.params;
-  const project = projects.find(p => p.id === id);
+  const project = projects.find(p => String(p.id) === id);
   if (!project) {
-    return res.status(400).json({ error: "Project not found" });
+    return res.status(400).json({ error: 'Project not found' });
   }
   return next();
 }
@@ -27,51 +27,51 @@ function checkProjectExists(req, res, next) {
 function newProject(req, res, next) {
   const { id, title } = req.body;
   if (!id) {
-    return res.status(400).json({ error: "Id required." });
+    return res.status(400).json({ error: 'Id required.' });
   }
   if (!title) {
-    return res.status(400).json({ erro: "Required title." });
+    return res.status(400).json({ error: 'Required title.' });
   }
-  const project = projects.find(p => p.id === id);
+  const project = projects.find(p => String(p.id) == id);
   if (project) {
-    return res.status(400).json({ error: "This project already exists." });
+    return res.status(400).json({ error: 'This project already exists.' });
   }
   return next();
 }
 //listando todos os projetos
-server.get("/projects", (req, res) => {
-  return res.json(req.projects);
+server.get('/projects', (req, res) => {
+  return res.json(projects);
 });
 //cadastrando novos projetos
-server.post("/projects", newProject, (req, res) => {
+server.post('/projects', newProject, (req, res) => {
   const { id, title } = req.body;
-  const tasks = [];
-  projects.push(id, title, tasks);
-  return res.json({ id, title, tasks });
+  const project = { id, title, tasks: [] };
+  projects.push(project);
+  return res.json(project);
 });
 //alterando projeto
-server.put("/projects/:id", checkProjectExists, (req, res) => {
+server.put('/projects/:id', checkProjectExists, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   if (!title) {
-    return res.status(400).json({ error: "Required title." });
+    return res.status(400).json({ error: 'Required title.' });
   }
-  const project = projects.find(p => p.id === id);
+  const project = projects.find(p => String(p.id) === id);
   project.title = title;
   return res.json(project);
 });
 //excluindo projeto
-server.delete("/projects/:id", checkProjectExists, (req, res) => {
+server.delete('/projects/:id', checkProjectExists, (req, res) => {
   const { id } = req.params;
-  const project = projects.find(p => p.id === id);
+  const project = projects.findIndex(p => String(p.id) === id);
   projects.splice(project.id, 1);
   return res.send();
 });
 //incluindo as tarefas no projeto
-server.post("/projects/:id/tasks", checkProjectExists, (req, res) => {
+server.post('/projects/:id/tasks', checkProjectExists, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  const project = project.find(p => p.id === id);
+  const project = projects.find(p => String(p.id) === id);
   project.tasks.push(title);
   return res.json(project);
 });
